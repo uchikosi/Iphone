79067a7a77ea5d6d255f72d349be712e3adb96d0
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2023-12-30 14:51:40 +0900

    ほぼ完成

---
+++ src/Android.java
@@ -0,0 +1,15 @@
+
+public class Android {
+	public static void main(String[] args){
+		SmartPhone Android = new SmartPhone();
+		Android.play();
+		Android.stop();
+		Android.next();
+		Android.back();
+		Android.call();
+		Android.mail();
+		Android.photo();
+		Android.internet();
+//		変数名.(ドット) メソッド名: 各変数に格納されたインスタンスが持つメソッドを呼び出す
+	}
+}
--- src/Iphone.java
+++ src/Iphone.java
@@ -1,4 +1,15 @@
 
-public class Iphone {
-
+public class Iphone{
+public static void main(String[] args){
+	SmartPhone iphone = new SmartPhone();
+	iphone.play();
+	iphone.stop();
+	iphone.next();
+	iphone.back();
+	iphone.call();
+	iphone.mail();
+	iphone.photo();
+	iphone.internet();
+//	変数名.(ドット) メソッド名: 各変数に格納されたインスタンスが持つメソッドを呼び出す
+	}
 }
